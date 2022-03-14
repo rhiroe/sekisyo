e23ae160c87bb076143d8bf7c3c46de109427037
rhiroe <ride.poke@gmail.com>
2022-03-14 13:39:40 +0900

    Define Hash#slice and Hash#except when ruby version < 3.0

--- lib/sekisyo.rb
+++ lib/sekisyo.rb
@@ -9,6 +9,25 @@ require_relative 'sekisyo/configuration'
 require_relative 'sekisyo/whitelist'
 require_relative 'sekisyo/middleware'
 
+if RUBY_VERSION < '3.0'
+  #
+  # Define Hash#except and Hash#slice since they do not exist less than Ruby 3.0.
+  #
+  class Hash
+    def except(*keys)
+      hash = dup
+      keys.each(&method(:delete))
+      hash
+    end
+
+    def slice(*keys)
+      hash = self.class.new
+      keys.each { |k| hash[k] = self[k] if has_key?(k) }
+      hash
+    end
+  end
+end
+
 module Sekisyo
   class WhitelistDefinitionError < StandardError; end
 end
